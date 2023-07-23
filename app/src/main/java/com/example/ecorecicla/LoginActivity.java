e6b310aafb39a6481a66332ef4fb618c9d938b4a
DESKTOP-5QP63V4\USUARIO <l.f.d.sando@gmai.com>
2023-07-23 16:22:26 -0500

    ER-5 FEAT: Section statidistics

--- app/src/main/java/com/example/ecorecicla/LoginActivity.java
+++ app/src/main/java/com/example/ecorecicla/LoginActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button login;
+    TextView register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
