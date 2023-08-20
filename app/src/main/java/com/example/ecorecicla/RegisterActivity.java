787f4611198888ada977518eafe744a6d814f6a1
DESKTOP-5QP63V4\USUARIO <l.f.d.sando@gmai.com>
2023-08-20 09:40:51 -0500

    ER-18 FEAT: Chnage in design

--- app/src/main/java/com/example/ecorecicla/RegisterActivity.java
+++ app/src/main/java/com/example/ecorecicla/RegisterActivity.java
@@ -6,25 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
 
     Button register;
+    EditText name, email, pass, user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         register=findViewById(R.id.btnRegister);
+        name=findViewById(R.id.editTextUser);
+        email=findViewById(R.id.editTextEmail);
+        user=findViewById(R.id.editTextUser);
+        pass= findViewById(R.id.editTextPass);
 
         //Creación de intensiones re-diressión de elementos
 
         Intent login= new Intent(this, LoginActivity.class);
 
         register.setOnClickListener(new View.OnClickListener() {
+
+
             @Override
             public void onClick(View v) {
+
+                //Validación de datos completos
+
+                if(!name.getText().toString().isEmpty() &&
+                        !email.getText().toString().isEmpty() &&
+                        !user.getText().toString().isEmpty() &&
+                        !pass.getText().toString().isEmpty()){
+
+                }
                 startActivity(login);
+
+
             }
         });
     }
---
+++ app/src/main/java/com/example/ecorecicla/modelos/user.java
@@ -0,0 +1,44 @@
+package com.example.ecorecicla.modelos;
+
+public class user {
+    String name,userName,email,password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public user(String name, String userName, String email, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+}
