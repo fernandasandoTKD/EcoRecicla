eed04e5c16499beef14cd40f5259ccf89ad6dc1c
DESKTOP-5QP63V4\USUARIO <l.f.d.sando@gmai.com>
2023-08-22 08:25:27 -0500

    ER-19 FEAT: Design first advice

--- app/src/main/java/com/example/ecorecicla/LoginActivity.java
+++ app/src/main/java/com/example/ecorecicla/LoginActivity.java
@@ -1,89 +1,89 @@
-package com.example.ecorecicla;
+    package com.example.ecorecicla;
 
-import androidx.appcompat.app.AppCompatActivity;
+    import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Filter;
-import android.widget.TextView;
-import android.widget.Toast;
+    import android.annotation.SuppressLint;
+    import android.content.Intent;
+    import android.os.Bundle;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.Filter;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-import com.example.ecorecicla.modelos.User;
+    import com.example.ecorecicla.modelos.User;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.ArrayList;
+    import java.io.BufferedReader;
+    import java.io.File;
+    import java.io.FileReader;
+    import java.util.ArrayList;
 
-public class LoginActivity extends AppCompatActivity {
+    public class LoginActivity extends AppCompatActivity {
 
-    Button login;
-    TextView register;
+        Button login;
+        TextView register;
 
-    EditText userLogin, passLogin;
+        EditText userLogin, passLogin;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login);
 
-        // Section of redirect  to other page
-        login =findViewById(R.id.btnRegister);
-        register=findViewById(R.id.txtRegister);
-        userLogin=findViewById(R.id.editTextUserLogin);
-        passLogin=findViewById(R.id.editTextTextPassLogin);
+            // Section of redirect  to other page
+            login =findViewById(R.id.btnRegister);
+            register=findViewById(R.id.txtRegister);
+            userLogin=findViewById(R.id.editTextUserLogin);
+            passLogin=findViewById(R.id.editTextTextPassLogin);
 
-        Intent home= new Intent(this, HomeActivity.class);
-        Intent registerSection= new Intent(this, RegisterActivity.class);
+            Intent home= new Intent(this, HomeActivity.class);
+            Intent registerSection= new Intent(this, RegisterActivity.class);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!userLogin.getText().toString().isEmpty() &&
-                !passLogin.getText().toString().isEmpty()){
-                    if(checkUser(userLogin.getText().toString(),passLogin.getText().toString())){
-                        startActivity(home);
-                    }else {
-                        Toast.makeText(getApplicationContext(),"Usuario o contraseña no validos",Toast.LENGTH_LONG).show();
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!userLogin.getText().toString().isEmpty() &&
+                    !passLogin.getText().toString().isEmpty()){
+                        if(checkUser(userLogin.getText().toString(),passLogin.getText().toString())){
+                            startActivity(home);
+                        }else {
+                            Toast.makeText(getApplicationContext(),"Usuario o contraseña no validos",Toast.LENGTH_LONG).show();
+                        }
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Los campos deben estar completos",Toast.LENGTH_LONG).show();
                     }
-                }else{
-                    Toast.makeText(getApplicationContext(),"Los campos deben estar completos",Toast.LENGTH_LONG).show();
                 }
-            }
-        });
+            });
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v ) {
-                startActivity(registerSection);
-            }
-        });
-
-    }
-
-    public  boolean checkUser(String user, String pass){
-        File file= new File(getFilesDir(), "userData.txt");
-        try{
-            BufferedReader reader= new BufferedReader(new FileReader(file));
-            String line;
-            while((line=reader.readLine())!= null){
-                String [] userData=line.split(",");;
-                String email =userData[1];
-                String userName=userData[2];
-                String password=userData[3];
-
-                if(email.equals(user) || userName.equals(user) && password.equals(pass)){
-                        return true;
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v ) {
+                    startActivity(registerSection);
                 }
-            }
-        }catch (Exception e){
-            e.printStackTrace();
+            });
+
         }
-        return false;
+
+        public  boolean checkUser(String user, String pass){
+            File file= new File(getFilesDir(), "userData.txt");
+            try{
+                BufferedReader reader= new BufferedReader(new FileReader(file));
+                String line;
+                while((line=reader.readLine())!= null){
+                    String [] userData=line.split(",");;
+                    String email =userData[1];
+                    String userName=userData[2];
+                    String password=userData[3];
+
+                    if(email.equals(user) || userName.equals(user) && password.equals(pass)){
+                            return true;
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return false;
+        }
     }
-}
--- app/src/main/java/com/example/ecorecicla/RecommendationActivity.java
+++ app/src/main/java/com/example/ecorecicla/RecommendationActivity.java
@@ -6,18 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TableLayout;
+
+import com.example.ecorecicla.modelos.Aluminum;
+import com.example.ecorecicla.modelos.Cardboard;
+import com.example.ecorecicla.modelos.Paper;
+import com.example.ecorecicla.modelos.Plastic;
+
+import java.util.List;
 
 public class RecommendationActivity extends AppCompatActivity {
 
     ImageView home;
+    TableLayout tableRecommendation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendation);
 
         home=findViewById(R.id.ivHome);
+        tableRecommendation=findViewById(R.id.TableRecommendation);
 
         Intent homeSection= new Intent(this, HomeActivity.class);
+
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +36,9 @@ public class RecommendationActivity extends AppCompatActivity {
                 startActivity(homeSection);
             }
         });
+    }
+
+    public void inflateTable(List<Aluminum> aluminnum, List <Cardboard> cardboard, List <Paper> paper, List <Plastic> plastic){
+
     }
 }
